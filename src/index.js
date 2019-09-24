a465ec4858b7eed7463630b923842f72664ae162
Syed Nazmus Sakib <contact@snsakib.com>
2019-09-24 11:24:42 +0600

    load images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import Logo from './Logo.svg';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
     element.classList.add('hello');
+
+    const myLogo = new Image();
+    myLogo.scr = Logo;
+
+    element.appendChild(myLogo);
 
     return element;
 }
